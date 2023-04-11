6458d0d2a2973d18759b6b2fc4e616f0f5a0110e
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-11 21:30:16 +0200

    Add structure html and operation index

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
-const strictEquals = (valueA, valueB) => {
-  if (Number.isNaN(valueA, NaN) && Number.isNaN(valueB, NaN)) {
-    return false;
-  }
-  if (Object.is(valueA, 0) && Object.is(valueB, -0)) {
-    return true;
-  }
-  if (Object.is(valueA, -0) && Object.is(valueB, 0)) {
-    return true;
-  }
-};
+import strictEquals from "./modules/index.js";
+
+console.log(`The result of 1 equals 1 is: ${strictEquals(1, 1)}`);
+console.log(`The result of NaN equals NaN is: ${strictEquals(NaN, NaN)}`);
+console.log(`The result of 0 equals -0 is: ${strictEquals(0, -0)}`);
+console.log(`The result of -0 equals 0 is: ${strictEquals(-0, 0)}`);
+console.log(`The result of 1 equals "1" is: ${strictEquals(1, "1")}`);
+console.log(`The result of true equals false is: ${strictEquals(true, false)}`);
+console.log(
+  `The result of false equals false is: ${strictEquals(false, false)}`
+);
+console.log(
+  `The result of "water" equals "oil" is: ${strictEquals("water", "oil")}`
+);
